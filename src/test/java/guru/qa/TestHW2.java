19037d0851e9514895cc5d136e4f6f0396da5ee0
AndreyTsGit <andreytseporinda@gmail.com>
2023-12-28 10:58:19 +0300

    added "TestHW2.1"

--- src/test/java/guru/qa/TestHW2.java
+++ src/test/java/guru/qa/TestHW2.java
@@ -1,38 +1,37 @@
 package guru.qa;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestHW2 {
 
     @Test
     void successfulTest(){
         open("https://demoqa.com/automation-practice-form");
-        Configuration.holdBrowserOpen = true;
-        $("[id=firstName]").setValue("Andrey");
-        $("[id=lastName]").setValue("Ts");
-        $("[id=userEmail]").setValue("ats.73@mail.ru");
+        $("[id=#firstName]").setValue("Andrey");
+        $("[id=#lastName]").setValue("Ts");
+        $("[id=#userEmail]").setValue("ats.73@mail.ru");
 
         $(".custom-control-label").click();
-        $("[id=userNumber]").setValue("");
-        $("[id=dateOfBirthInput]").click();
+        $("[id=#userNumber]").setValue("");
+        $("[id=#dateOfBirthInput]").click();
 
-        $(".react-datepicker__month-select").selectOption("June");
-        $(".react-datepicker__year-select").selectOption("1973");
-        $(".react-datepicker__day--22").click();
+        $(".react-date picker__month-select").selectOption("June");
+        $(".react-date picker__year-select").selectOption("1973");
+        $(".react-date picker__day--22").click();
 
-        $("[id=subjectsInput]").setValue("a").click();
+        $("[id=#subjectsInput]").setValue("a").click();
         $("label[for='hobbies-checkbox-1']").click();
         $("label[for='hobbies-checkbox-2']").click();
         $("label[for='hobbies-checkbox-3']").click();
 
-        $("[id=currentAddress]").setValue("123, область, город, улица д.1, кв.1");
-        $("[id=react-select-3-input]").setValue("AB").click(ENTER);
-        $("[id=react-select-4-input]").setValue("BC").click();
+        $("[id=#currentAddress]").setValue("123, область, город, улица д.1, кв.1");
+        $("[id=#react-select-3-input]").setValue("AB").click();
+        $("[id=#react-select-4-input]").setValue("BC").click();
 
-        $("[id=submit").click();
+        $("[id=#submit").pressEnter();
 
 
 
